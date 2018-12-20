831c56ae65b5c5d1c37923aa718796d3e79ca4c7
Juan Pablo Almonacid <jalmonacid@artear.com>
2018-12-20 17:42:52 -0300

    Video #11 Events, refs and 'this' binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,30 @@ import React, { Fragment } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    // Or alternatively,  myInput = React.createRef(); as class property.
+    constructor(props) {
+        super(props);
+        this.myInput = React.createRef();
+    }
+
+    goToStore = event => {
+        event.preventDefault();
+        console.log(this);
+    };
+
     render() {
         return (
             <Fragment>
                 <p>Fish!</p>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                    <input
+                        type="text"
+                        ref={this.myInput}
+                        required
+                        placeholder="Store Name"
+                        defaultValue={getFunName()}
+                    />
                     <button type="submit">Visit Store → </button>
                 </form>
             </Fragment>
