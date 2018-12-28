23a0fdbeb2487ac79827b8dd8be6cd7e3e8f0d0b
Juan Pablo Almonacid <jalmonacid@artear.com>
2018-12-28 15:20:53 -0300

    Video #17 Displaying order state with JSX

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-    render() {
-        return (
-            <div className="inventory">
-                Order!!!
-            </div>
-        );
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === 'available';
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : 'fish'} is no longer available
+        </li>
+      );
     }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name} {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
+  render() {
+    const orderIds = Object.keys(this.props.order);
+
+    const total = orderIds.reduce((total, orderId) => {
+      const fish = this.props.fishes[orderId];
+      const count = this.props.order[orderId];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return total + (count * fish.price);
+      }
+      return total;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+        </ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Order;
