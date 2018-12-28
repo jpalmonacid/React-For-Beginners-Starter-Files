5008e059c2d050ceeb6f2dd87d58de39e561a65e
Juan Pablo Almonacid <jalmonacid@artear.com>
2018-12-28 17:46:37 -0300

    Use environment variables for firebase API configuration

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,10 +1,13 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
+const { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_AUTH_DOMAIN, REACT_APP_FIREBASE_DB_URL } = process.env;
+console.log(process.env);
+
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCAp3CJj1_QnmCPf2ty62Jobi6E2xnq_D4",
-  authDomain: "catch-of-the-day-jpa.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-jpa.firebaseio.com"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DB_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
